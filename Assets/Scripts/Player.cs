e8b2456a3c1164cb6930b4c1cb3ee3239601c1fb
web3ten0 <web3ten0@gmail.com>
2022-09-23 22:32:49 +0900

    Playerの移動

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,65 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _walk_speed = 8f;
+
+    private PlayerInput _input;
+    private CharacterController _characon;
+    private Vector2 _move_direction;
+    private Vector2 _look_direction;
+
+    private void Awake()
     {
-        
+        Debug.Log("Awake");
+        TryGetComponent(out _input);
+        TryGetComponent(out _characon);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        Debug.Log("OnEnable");
+        _input.actions["Move"].performed += OnMove;
+        _input.actions["Move"].canceled += OnMove;
+        _input.actions["Look"].performed += OnLook;
+        _input.actions["Fire"].started += OnFire;
     }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        _input.actions["Move"].performed -= OnMove;
+        _input.actions["Move"].canceled -= OnMove;
+        _input.actions["Look"].started -= OnLook;
+        _input.actions["Fire"].started -= OnFire;
+    }
+
+    private void OnMove(InputAction.CallbackContext obj)
+    {
+        _move_direction = obj.ReadValue<Vector2>();
+    }
+
+    private void OnLook(InputAction.CallbackContext obj)
+    {
+        _look_direction = obj.ReadValue<Vector2>();
+    }
+
+    private void OnFire(InputAction.CallbackContext obj)
+    {
+        Debug.Log("on fire");
+    }
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(_move_direction.x, 0, _move_direction.y).normalized;
+        _characon.Move(direction * _walk_speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
 }
